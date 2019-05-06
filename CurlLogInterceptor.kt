534ad019ce5c26bc1e1dd0d58c6099712c8c1759
Jallu <jallumaxime@gmail.com>
2019-05-06 11:33:11 +0200

    Update CurlLogInterceptor.kt

--- CurlLogInterceptor.kt
+++ CurlLogInterceptor.kt
@@ -9,7 +9,7 @@ import java.nio.charset.Charset
 private val UTF8 = Charset.forName("UTF-8")
 private const val SINGLE_DIVIDER = "────────────────────────────────"
 
-class CurlLogInterceptor(private val logger: Logger?, _tag: String = "CURL") : Interceptor {
+class CurlLogInterceptor(private val logger: Logger? = null, _tag: String = "CURL") : Interceptor {
 
     private val tag = if (_tag.startsWith("CURL")) {
         _tag
@@ -21,58 +21,62 @@ class CurlLogInterceptor(private val logger: Logger?, _tag: String = "CURL") : I
     @Throws(IOException::class)
     override fun intercept(chain: Interceptor.Chain): Response {
         val request = chain.request()
-        if (request.body() != null) {
-            curlCommandBuilder = StringBuilder("")
-            // add cURL command
-            curlCommandBuilder.append("cURL ")
-            curlCommandBuilder.append("-X ")
-            // add method
-            curlCommandBuilder.append("${request.method().toUpperCase()} ")
-            // adding headers
-            for (headerName in request.headers().names()) {
-                addHeader(headerName, request.headers().get(headerName)!!)
-            }
 
+        curlCommandBuilder = StringBuilder("")
+        // add cURL command
+        curlCommandBuilder.append("cURL ")
+        curlCommandBuilder.append("-X ")
+        // add method
+        curlCommandBuilder.append("${request.method().toUpperCase()} ")
+        // adding headers
+        for (headerName in request.headers().names()) {
+            addHeader(headerName, request.headers()[headerName])
+        }
+        request.body()?.also {
             // adding request body
-            val requestBody = request.body()!!
             if (request.body() != null) {
                 val buffer = Buffer()
-                requestBody.writeTo(buffer)
-                var charset = UTF8
-                val contentType = requestBody.contentType()
+                it.writeTo(buffer)
+                val charset = UTF8
+                val contentType = it.contentType()
                 if (contentType != null) {
-                    addHeader("Content-Type", request?.body()!!.contentType().toString())
-                    charset = contentType.charset(UTF8)
+                    addHeader("Content-Type", it.contentType().toString())
                     curlCommandBuilder.append(" -d '${buffer.readString(charset)}'")
                 }
             }
-
-            // add request URL
-            curlCommandBuilder.append(" \"${request.url()}\"")
-            curlCommandBuilder.append(" -L")
-
-            print(request.url().toString(), curlCommandBuilder.toString())
         }
+        // add request URL
+        curlCommandBuilder.append(" \"${request.url()}\"")
+        curlCommandBuilder.append(" -L")
+
+        print(request.url().toString(), curlCommandBuilder.toString())
+
         return chain.proceed(request)
     }
 
-    private fun addHeader(headerName: String, headerValue: String) {
-        curlCommandBuilder.append("-H \"$headerName: $headerValue\" ")
+    private fun addHeader(headerName: String, headerValue: String?) {
+        try {
+            headerValue?.also {
+                curlCommandBuilder.append("-H \"$headerName: $it\" ")
+            }
+        } catch (e: ArrayIndexOutOfBoundsException) {
+            log(e.message ?: "CURL - ArrayIndexOutOfBoundsException")
+        }
     }
 
-
     private fun print(url: String, msg: String) {
-        val logMsg = StringBuilder("\n")
-        logMsg.append("\n")
-        logMsg.append("URL: $url")
-        logMsg.append("\n")
-        logMsg.append(SINGLE_DIVIDER)
-        logMsg.append("\n")
-        logMsg.append(msg)
-        logMsg.append(" ")
-        logMsg.append(" \n")
-        logMsg.append(SINGLE_DIVIDER)
-        logMsg.append(" \n ")
+        val logMsg = StringBuilder("\n").apply {
+            appendln()
+            append("URL: $url")
+            appendln()
+            append(SINGLE_DIVIDER)
+            appendln()
+            append(msg)
+            append(" ")
+            appendln()
+            append(SINGLE_DIVIDER)
+            appendln()
+        }
 
         log(logMsg.toString())
     }
