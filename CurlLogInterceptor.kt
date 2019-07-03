1c0b40d0f9f47485b6c3b2b0580b60247076a85c
Jallu <jallumaxime@gmail.com>
2019-07-03 18:05:20 +0200

    Update CurlLogInterceptor.kt

--- CurlLogInterceptor.kt
+++ CurlLogInterceptor.kt
@@ -5,92 +5,76 @@ import okio.Buffer
 import java.io.IOException
 import java.nio.charset.Charset
 
-
-private val UTF8 = Charset.forName("UTF-8")
-private const val SINGLE_DIVIDER = "────────────────────────────────"
-
-class CurlLogInterceptor(private val logger: Logger? = null, _tag: String = "CURL") : Interceptor {
-
-    private val tag = if (_tag.startsWith("CURL")) {
-        _tag
-    } else {
-        "CURL-$_tag"
+class CurlLogInterceptor : Interceptor {
+    companion object {
+        private val UTF8 = Charset.forName("UTF-8")
+        private const val SINGLE_DIVIDER = "────────────────────────────────────────────────────────────────"
     }
-    private var curlCommandBuilder = StringBuilder("")
 
     @Throws(IOException::class)
     override fun intercept(chain: Interceptor.Chain): Response {
         val request = chain.request()
+        if (!BuildConfig.DEBUG) {
+            return chain.proceed(request)
+        }
 
-        curlCommandBuilder = StringBuilder("")
-        // add cURL command
-        curlCommandBuilder.append("cURL ")
-        curlCommandBuilder.append("-X ")
-        // add method
-        curlCommandBuilder.append("${request.method().toUpperCase()} ")
+        val logBuilder = StringBuilder("curl -X ${request.method().toUpperCase()} ")
+
         // adding headers
         for (headerName in request.headers().names()) {
-            addHeader(headerName, request.headers()[headerName])
+            addHeader(headerName, request.headers()[headerName])?.also {
+                logBuilder.append(it)
+            }
         }
         request.body()?.also {
             // adding request body
-            if (request.body() != null) {
-                val buffer = Buffer()
-                it.writeTo(buffer)
-                val charset = UTF8
-                val contentType = it.contentType()
-                if (contentType != null) {
-                    addHeader("Content-Type", it.contentType().toString())
-                    curlCommandBuilder.append(" -d '${buffer.readString(charset)}'")
-                }
+            val buffer = Buffer()
+            it.writeTo(buffer)
+
+            val contentType = it.contentType()
+            if (contentType != null) {
+                addHeader("Content-Type", it.contentType().toString())
+                logBuilder.append(" -d '${buffer.readString(UTF8)}'")
             }
         }
         // add request URL
-        curlCommandBuilder.append(" \"${request.url()}\"")
-        curlCommandBuilder.append(" -L")
+        logBuilder.appendln().append(" \"${request.url()}\"")
+        logBuilder.append(" -L")
 
-        print(request.url().toString(), curlCommandBuilder.toString())
+        print(request.url().toString(), logBuilder.toString())
 
         return chain.proceed(request)
     }
 
-    private fun addHeader(headerName: String, headerValue: String?) {
-        try {
-            headerValue?.also {
-                curlCommandBuilder.append("-H \"$headerName: $it\" ")
+    private fun addHeader(headerName: String, headerValue: String?): String? {
+        return try {
+            headerValue?.let {
+                "-H \"$headerName: $it\" "
             }
         } catch (e: ArrayIndexOutOfBoundsException) {
             log(e.message ?: "CURL - ArrayIndexOutOfBoundsException")
+            null
         }
     }
 
     private fun print(url: String, msg: String) {
         val logMsg = StringBuilder("\n").apply {
-            appendln()
-            append("URL: $url")
-            appendln()
             append(SINGLE_DIVIDER)
             appendln()
+            append("Request ID: " + this@CurlLogInterceptor.toString().substringAfterLast("."))
+            appendln()
+            append("URL cible: $url")
+            appendln()
+            appendln()
             append(msg)
-            append(" ")
             appendln()
             append(SINGLE_DIVIDER)
             appendln()
         }
-
-        log(logMsg.toString())
+        log(logMsg.toString().replace("-H", "\r\n-H"))
     }
 
     private fun log(msg: String) {
-        if (logger != null) {
-            logger.print(tag, msg)
-        } else {
-            Log.i(tag, msg)
-        }
+        Log.i(TAG, msg)
     }
-
-}
-
-interface Logger {
-    fun print(tag: String, msg: String)
 }
